8200192617f6b7514ab6ad8191c8777aebb52c59
Titus <165676672+half-pace@users.noreply.github.com>
2026-09-24 16:53:45 +0530

    Regression test 2 done

--- script_ingestion/tests.py
+++ script_ingestion/tests.py
@@ -6,26 +6,26 @@ from cleaning import normalize_whitespace
 from knowledge.section import create_sections
 
 
-# raw_folder = Path("knowledge_base/01_raw_data")
-# manifest_path = Path("knowledge_base/document_manifest.json")
+raw_folder = Path("knowledge_base/01_raw_data")
+manifest_path = Path("knowledge_base/document_manifest.json")
 
-# run_ingestion(raw_folder, manifest_path)
+run_ingestion(raw_folder, manifest_path)
 
 #temporary
-path = Path("knowledge_base/01_raw_data/processes/Traditionalweaving_Process.pdf")
+# path = Path("knowledge_base/01_raw_data/processes/Traditionalweaving_Process.pdf")
 
-raw_content = extract_content(path)
-cleaned_content = normalize_whitespace(raw_content)
-# for line in cleaned_content.splitlines()[:15]:
-#     print(repr(line))
-print("Extracted Characters: ", len(cleaned_content))
+# raw_content = extract_content(path)
+# cleaned_content = normalize_whitespace(raw_content)
+# # for line in cleaned_content.splitlines()[:15]:
+# #     print(repr(line))
+# print("Extracted Characters: ", len(cleaned_content))
 
-sections = create_sections(cleaned_content)
-print("Number of sections: ", len(sections))
+# sections = create_sections(cleaned_content)
+# print("Number of sections: ", len(sections))
 
-# normalized_document = process_document(path, "processes/Traditionalweaving_Process.pdf")
+# # normalized_document = process_document(path, "processes/Traditionalweaving_Process.pdf")
 
-for section in sections:
-    print(section.title)
-    print(section.content[:200])
-    print("-" * 50)
+# for section in sections:
+#     print(section.title)
+#     print(section.content[:200])
+#     print("-" * 50)

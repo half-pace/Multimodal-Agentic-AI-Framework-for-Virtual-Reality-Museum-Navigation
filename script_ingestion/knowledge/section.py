a8f20f6104c4336c203150927ebdb4b7c249050b
Titus <165676672+half-pace@users.noreply.github.com>
2026-09-24 12:10:50 +0530

    test1: create_sections function in section.py testin1 completed

--- script_ingestion/knowledge/section.py
+++ script_ingestion/knowledge/section.py
@@ -1,9 +1,21 @@
 """Imports"""
-from dataclasses import dataclass
+from dataclasses import dataclass 
 
 """Dataclass structure"""
 @dataclass
 class DocumentSection:
     title: str | None
     content: str
+
+def create_sections(text: str) -> list[DocumentSection]:
+    """Creates sections from cleaned texts"""
     
+    return [DocumentSection(
+        title=None,
+        content=text
+    )]
+
+text = "Traditional weaving begins with preparation of the yarn."
+
+sections = create_sections(text)
+print(sections)

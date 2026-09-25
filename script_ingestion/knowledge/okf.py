18f0c394a6556694a9426faa3d3aef50e71828ba
Titus <165676672+half-pace@users.noreply.github.com>
2026-09-25 12:43:02 +0530

    create_slug function works in okf.py

--- script_ingestion/knowledge/okf.py
+++ script_ingestion/knowledge/okf.py
@@ -1,5 +1,13 @@
 """Imports"""
-
+import re
 
 
 """Functions"""
+def create_slug(title: str) -> str:
+    """Converts a title into a URL/filename-friendly slug."""
+    new_title = re.sub(r"[^\w\d]+", "-", title)
+    return new_title.lower().strip("-")
+
+print(create_slug("Ginning"))
+print(create_slug("Traditional Weaving Process"))
+print(create_slug("Eri Silk - Reeling"))
